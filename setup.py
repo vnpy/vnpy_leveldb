b06907fbf2d251c380bba1f3e84b466befe56efb
noranhe <63988546+noranhe@users.noreply.github.com>
2021-10-21 17:18:08 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup
-import platform
 
-if platform.system() == "Windows":
-    setup(install_requires = "plyvel-win32")
-elif platform.system() == "Linux":
-    setup(install_requires = "plyvel")
+setup()
